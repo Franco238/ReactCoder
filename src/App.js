00dff543ee197d9183d94c31973997eff90f3b36
Franco Duek <francoduek@gmail.com>
2022-08-22 15:58:37 -0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Tienda oficial de la Asociación del Fútbol Argentino
-        </p>
-        <a
-          className="App-link"
-          href="https://www.afa.com.ar/es/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Visita nuestro sitio oficial
-        </a>
-      </header>
-    </div>
+    <Navbar/>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,39 @@
+
+const Navbar = () => {
+
+return(
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">AFA STORE</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Qatar 2022</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Nuestra historia</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Donde estamos</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Productos
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Todos</a></li>
+            <li><a class="dropdown-item" href="#">Productos destacados</a></li>
+            <li><a class="dropdown-item" href="#">Mundial Qatar 2022</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+);
+}
+
+export default Navbar
