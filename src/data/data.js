21d3728e5fb6f763f8971f197aaf8427ee861d63
Franco Duek <francoduek@gmail.com>
2022-08-31 14:26:24 -0300

    agregue el stock

---
+++ src/data/data.js
@@ -0,0 +1,39 @@
+export const stock = [
+
+    {
+        id: 1,
+        nombre: "Camiseta titular 2022",
+        description: "Camiseta oficial de la seleccion argentina para el mundial Qatar 2022",
+        img: "./img/camisetaMessi.jpg.crdownload",
+        price: "15.000",
+        stock: 200,
+    },
+
+    {
+        id: 2,
+        nombre: "Camiseta suplente 2022",
+        description: "Camiseta violeta de la seleccion argentina para el mundial en Qatar 2022",
+        img: "./img/camisetaMessiSuplente.jpg",
+        price: "14.000",
+        stock: 300,
+    },
+
+    {
+        id: 3,
+        nombre: "Campera AFA 2022",
+        description: "Campera oficial de la seleccion argentina 2022",
+        img: "./img/camperaAfa.jpg",
+        price: "22000",
+        stock: 80,
+    },
+
+    {
+        id: 4,
+        nombre: "Pantalon AFA 2022",
+        description: "Pantalon de presentación oficial de la seleccion argentina 2022",
+        img: "./img/pantalonAfa.jpg",
+        price: "11000",
+        stock: 120,
+    }
+
+]
